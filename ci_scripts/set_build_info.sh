a7af31eed74608be2825a74aada2a2c2835db66c
Andy Gallagher <andy.gallagher@theguardian.com>
2019-03-13 12:42:45 +0000

    adding script to put branch and build number into the version

---
+++ ci_scripts/set_build_info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "${BUILD_NUMBER}" == "" ]; then
+	declare -x BUILD_NUMBER=DEV
+fi
+
+if [ "${BUILD_BRANCH}" == "" ]; then
+	declare -x BUILD_BRANCH=$(git branch --no-color | grep -E '^\*' | cut -c 3-)
+fi
+
+if [ "${BUILD_BRANCH}" == "master" ]; then
+	declare -x BRANCH_SUFFIX=""
+else
+	declare -x BRANCH_SUFFIX=" from ${BUILD_BRANCH}"
+fi
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion Build ${BUILD_NUMBER}${BRANCH_SUFFIX}" "ArchiveHunterDownloadManager/Info.plist"
+
